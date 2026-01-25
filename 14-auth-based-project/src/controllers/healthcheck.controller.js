de3a82cf0056265cd1c56fb0c36bd73aa62be323
sarfaraz <sarfarazgithub@gmail.com>
2026-01-25 23:55:32 +0530

    healthcheck-controller

---
+++ 14-auth-based-project/src/controllers/healthcheck.controller.js
@@ -0,0 +1,10 @@
+import asyncHandler from "../utils/async-handler.js";
+import { ApiResponse } from "../utils/api-response.js";
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(
+    new ApiResponse(200, { message: "success" })
+  );
+});
+
+export default healthCheck;
